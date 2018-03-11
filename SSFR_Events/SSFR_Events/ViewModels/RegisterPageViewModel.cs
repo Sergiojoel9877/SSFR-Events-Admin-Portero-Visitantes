364536cb67d6bc417a62abfca8df794ef2d67328
Sergio Joel Ferreras Batista <sergio.ferreras@isqwebsite.com>
2018-03-11 14:23:58 -0400

    Feat: Register Page agregado.

--- SSFR_Events/SSFR_Events/ViewModels/RegisterPageViewModel.cs
+++ SSFR_Events/SSFR_Events/ViewModels/RegisterPageViewModel.cs
@@ -1,11 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Xamarin.Forms;
 
 namespace SSFR_Events.ViewModels
 {
-    public class RegisterPageViewModel 
+    public class RegisterPageViewModel : ViewModelBase
     {
+        private string email;
+        public string Email
+        {
+            get => email;
 
+            set => SetProperty(ref email, value);
+        }
+
+        private string passWord;
+        public string PassWord
+        {
+            get => passWord;
+
+            set => SetProperty(ref passWord, value);
+        }
+
+        private string confirmPassWord;
+        public string ConfirmPassWord
+        {
+            get => confirmPassWord;
+
+            set => SetProperty(ref confirmPassWord, value);
+        }
+
+        private Command register;
+        public Command Register {
+
+            get => register ?? (register = new Command( () => {
+
+
+            }));
+        }
     }
 }
---
+++ SSFR_Events/SSFR_Events/Views/RegisterPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using SSFR_Events.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SSFR_Events.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class RegisterPage : ContentPage
+	{
+        RegisterPageViewModel ViewModel;
+
+		public RegisterPage ()
+		{
+			InitializeComponent ();
+
+            ViewModel = new RegisterPageViewModel();
+
+            BindingContext = ViewModel;
+		}
+	}
+}
