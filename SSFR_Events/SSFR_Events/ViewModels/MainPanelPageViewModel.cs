9e1757e14e459932e572c99b7027e30026b92795
Sergio Joel Ferreras Batista <sergio.ferreras@isqwebsite.com>
2018-03-17 21:03:47 -0400

    Refactor: MainPanelPage terminado.

--- SSFR_Events/SSFR_Events/ViewModels/MainPanelPageViewModel.cs
+++ SSFR_Events/SSFR_Events/ViewModels/MainPanelPageViewModel.cs
@@ -14,26 +14,46 @@ namespace SSFR_Events.ViewModels
         private ImageSource aniversaryImage = "aniversario.png";
         public ImageSource AniversaryImage
         {
-
             get => aniversaryImage;
 
             set => SetProperty(ref aniversaryImage, value);
-
         }
 
         private ImageSource marryageImage = "boda.png";
         public ImageSource MarryageImage
         {
-
             get => marryageImage;
 
             set => SetProperty(ref marryageImage, value);
+        }
+
+        private ImageSource birthdayImage = "cumpleanos.png";
+        public ImageSource BirthdayImage
+        {
+            get => birthdayImage;
+
+            set => SetProperty(ref birthdayImage, value);
+        }
+
+        private ImageSource funeralImage = "velatorio.png";
+        public ImageSource FuneralImage
+        {
+            get => funeralImage;
+
+            set => SetProperty(ref funeralImage, value);
+        }
+
+        private ImageSource personalizedImage = "cruz.png";
+        public ImageSource PersonalizedImage
+        {
+            get => personalizedImage;
+
+            set => SetProperty(ref personalizedImage, value);
         }
 
         public MainPanelPageViewModel(INavigation navService)
         {
             _navService = navService;
         }
-
     }
 }
