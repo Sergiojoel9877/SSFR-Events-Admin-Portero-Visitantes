ee40338d5c44c3892439f5de84ea48e741a9eae8
Sergio Joel Ferreras Batista <sergio.ferreras@isqwebsite.com>
2018-03-20 21:29:26 -0400

    Feat: Vista AddEvent aderido al proyecto. Feat: ViewModel de la vista AddEvent aderido al proyecto.

---
+++ SSFR_Events/SSFR_Events/ViewModels/AddEventViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace SSFR_Events.ViewModels
+{
+    public class AddEventViewModel : ViewModelBase
+    {
+        INavigation _navService;
+
+        public AddEventViewModel(INavigation navService)
+        {
+            _navService = navService;
+        }
+    }
+}
---
+++ SSFR_Events/SSFR_Events/Views/AddEvent.xaml.cs
@@ -0,0 +1,28 @@
+﻿using SSFR_Events.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SSFR_Events.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class AddEvent : ContentPage
+	{
+        AddEventViewModel ViewModel;
+
+		public AddEvent ()
+		{
+			InitializeComponent ();
+
+            ViewModel = new AddEventViewModel(Navigation);
+
+            BindingContext = ViewModel;
+            
+		}
+	}
+}
