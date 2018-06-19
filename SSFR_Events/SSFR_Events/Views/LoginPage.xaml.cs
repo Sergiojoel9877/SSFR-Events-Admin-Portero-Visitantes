85cd40368899e9bc9d9ff1e32033fa85f540f1d3
Sergio Joel Ferreras <sergio.Ferreras@isgwebsite.com>
2018-06-18 21:15:02 -0400

    Another Revert.

--- SSFR_Events/SSFR_Events/Views/LoginPage.xaml.cs
+++ SSFR_Events/SSFR_Events/Views/LoginPage.xaml.cs
@@ -39,18 +39,5 @@ namespace SSFR_Events.Views
                 ViewModel.IsChecked = false;
             }
         }
-
-        protected override async void OnAppearing()
-        {
-            base.OnAppearing();
-
-            await Splash.ScaleTo(1, 2000);
-            await Splash.ScaleTo(0.8, 1500, Easing.Linear);
-            await Splash.ScaleTo(40, 1000, Easing.Linear);
-            await Splash.FadeTo(20, 1000, Easing.Linear);
-            await Splash.FadeTo(0, 1000, Easing.Linear);
-            await Splash.ScaleTo(150, 700, Easing.Linear);
-
-        }
     }
 }
